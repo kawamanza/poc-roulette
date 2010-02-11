264cdf4273011b848cec53ca052438b8ca64cce3
Marcelo Manzan <manzan@gmail.com>
2010-02-11 18:44:19 -0200

    Refactoring: YAML loading; play-roulette => lib/roulette.rb

--- lib/roulette.rb
+++ lib/roulette.rb
@@ -1,3 +1,14 @@
 Dir.glob(File.join File.dirname(__FILE__), 'util', '*.rb').each do |file|
   require file
 end
+
+# Loading params
+config_file = "config/roulette.yml"
+unless File.exists? config_file
+  ccputs "{red}config file does not exists:", "{yellow}#{config_file}"
+  exit 1
+end
+require 'yaml'
+ccputs "{green}Loading #{config_file}"
+config = YAML::load_file config_file
+
